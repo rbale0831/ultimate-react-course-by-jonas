f41d9629c6c9d2dafc4d5b6abab8b34a429d37bc
rbalecfc <rohit.bale@cloudfirstgroup.com>
2024-05-01 01:40:34 +0530

    implementation of steps project

---
+++ 04-steps/steps/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+const App = () => {
+  const step = 2;
+  return (
+    <div className='steps'>
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+
+      <p className='message'>
+        Step {step}: {messages[step - 1]}
+      </p>
+
+      <div className="buttons">
+        <button style={{ backgroundClip: '#7950f2', color: "#fff" }}>Previous</button>
+        <button style={{ backgroundClip: '#7950f2', color: "#fff" }}>Next</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
